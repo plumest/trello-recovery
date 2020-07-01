cb0e8c3849ae604c4c212a8fd12044bcb4f89ec7
plumest <chayathon.k@ku.th>
2020-07-01 18:42:59 +0700

    deploy

---
+++ precache-manifest.32a1f2dbc58035865ffa1b2b2593587a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03191d0d5982d5fa5df1",
+    "url": "/trello-recovery/css/app.b2bd682d.css"
+  },
+  {
+    "revision": "b4690cb98a3033e7720ec8c79e2a728e",
+    "url": "/trello-recovery/index.html"
+  },
+  {
+    "revision": "03191d0d5982d5fa5df1",
+    "url": "/trello-recovery/js/app.d262579b.js"
+  },
+  {
+    "revision": "5d91880f10956130223d",
+    "url": "/trello-recovery/js/chunk-vendors.ce3dc4cd.js"
+  },
+  {
+    "revision": "9378e931d912f2814cd74b310eba2b61",
+    "url": "/trello-recovery/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trello-recovery/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/trello-recovery/precache-manifest.32a1f2dbc58035865ffa1b2b2593587a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "trello-recovery"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
